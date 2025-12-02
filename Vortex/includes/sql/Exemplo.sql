711c7d3231b6782256f32bbecdb8d93431345d20
Matheus <matheusrcm1@hotmail.com>
2025-12-02 15:14:54 -0300

    Melhoria dos Exemplos

--- Vortex/includes/sql/Exemplo.sql
+++ Vortex/includes/sql/Exemplo.sql
@@ -7,7 +7,22 @@ VALUES
 ('Matheus Reinhart Camargo Martins Catarino', '2781392513027', 'matheus.catarino@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
 ('Feliphe Eduardo Silvério Gonçalves de Oliveira', '2781392513020', 'feliphe.oliveira@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
 ('Marcos Vinícius Rocha', '2781392513032', 'marcos.rocha@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
-('Luis Gustavo Araujo Portirio', '2781392513031', 'luis.porfirio@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy');
+
+('Luis Gustavo Araujo Portirio', '2781392513031', 'luis.porfirio@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Mariana Duarte', '2781392513033', 'mariana.duarte@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Lucas Ferraz', '2781392513034', 'lucas.ferraz@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Gabriel Monteiro', '2781392513035', 'gabriel.monteiro@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Isabela Ramos', '2781392513036', 'isabela.ramos@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Rafael Cardoso', '2781392513037', 'rafael.cardoso@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Fernanda Nogueira', '2781392513038', 'fernanda.nogueira@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Eduardo Santana', '2781392513039', 'eduardo.santana@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Patrícia Gomes', '2781392513040', 'patricia.gomes@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Bruno Alcântara', '2781392513041', 'bruno.alcantara@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Carolina Vieira', '2781392513042', 'carolina.vieira@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('André Mendonça', '2781392513043', 'andre.mendonca@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Lívia Costa', '2781392513044', 'livia.costa@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Henrique Barros', '2781392513045', 'henrique.barros@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
+('Beatriz Antunes', '2781392513046', 'beatriz.antunes@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy');
 
 
 -- Valores adm
@@ -19,14 +34,19 @@ VALUES
 ('Marcos Vinícius Rocha', '2781392513032', 'marcos.rocha@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy'),
 ('Luis Gustavo Araujo Portirio', '2781392513031', 'luis.porfirio@fatec.sp.gov.br', '$2y$10$9OQvDx8Rk3XcfwzCjq4l0.Ng8RcQHj0ufF3GJp5YKv9pZxLJkS7cy');
 
+
 -- Valores votacao
 INSERT INTO votacao 
 (data_inicio, data_fim, data_inscricao, semestre, curso, descricao, status, id_adm)
 VALUES
 ('2025-12-12 08:00:00', '2025-12-20 18:00:00', '2025-12-11 12:00:00', '2', 'DSM', 'Votação para escolha de representante de turma', 'ativo', 2),
 ('2025-12-12 08:00:00', '2025-12-20 18:00:00', '2025-12-11 12:00:00', '5', 'GI', 'Votação para escolha de representante de turma', 'ativo', 3),
-('2025-11-10 08:00:00', '2025-12-20 18:00:00', '2025-11-9 12:00:00', '1', 'DSM', 'Votação para escolha de representante de turma', 'ativo', 1);
+('2025-11-10 08:00:00', '2025-12-20 18:00:00', '2025-11-9 12:00:00', '1', 'DSM', 'Votação para escolha de representante de turma', 'ativo', 1),
 
+('2025-12-15 08:00:00', '2025-12-22 18:00:00', '2025-12-14 12:00:00', '3', 'GI', 'Votação para escolha de representante de turma', 'ativo', 2),
+('2025-12-16 08:00:00', '2025-12-24 18:00:00', '2025-12-15 12:00:00', '4', 'GI', 'Votação para escolha de representante de turma', 'ativo', 1),
+('2025-12-17 08:00:00', '2025-12-25 18:00:00', '2025-12-16 12:00:00', '6', 'GE', 'Votação para escolha de representante de turma', 'ativo', 3),
+('2025-12-19 08:00:00', '2025-12-27 18:00:00', '2025-12-18 12:00:00', '1', 'GE', 'Votação para escolha de representante de turma', 'ativo', 1);
 
 --  candidatos de exemplo
 INSERT INTO candidato (id_aluno, descricao, email, foto)
@@ -34,15 +54,36 @@ VALUES
 (1, 'Aluno ativo na turma, candidato a representante', 'victor.almeida28@fatec.sp.gov.br', 'img/uploads/fotos/candidato2.png'),
 (5, 'Aluno engajado, deseja representar a turma', 'luis.porfirio@fatec.sp.gov.br', 'img/uploads/fotos/candidato5.png'),
 (3, 'Aluno participativo, candidato a representante', 'feliphe.oliveira@fatec.sp.gov.br', 'img/uploads/fotos/candidato6.png'),
-(4, 'Aluno exemplar, candidato a representante', 'marcos.rocha@fatec.sp.gov.br', 'img/uploads/fotos/candidato3.png');
+(4, 'Aluno exemplar, candidato a representante', 'marcos.rocha@fatec.sp.gov.br', 'img/uploads/fotos/candidato3.png'),
+
+(6, 'Aluno comprometido e sempre prestativo nas atividades de sala', 'mariana.duarte@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(7, 'Aluno comunicativo e com interesse em melhorar o ambiente acadêmico', 'lucas.ferraz@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(8, 'Aluno responsável e engajado em projetos internos', 'gabriel.monteiro@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(9, 'Aluna dedicada e disposta a representar a turma', 'isabela.ramos@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(10, 'Aluno motivado e participativo em atividades acadêmicas', 'rafael.cardoso@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(11, 'Aluna empenhada e focada no coletivo da sala', 'fernanda.nogueira@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(12, 'Aluno proativo e disposto a representar os colegas', 'eduardo.santana@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(13, 'Aluna organizada e engajada em causas estudantis', 'patricia.gomes@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(14, 'Aluno disciplinado e atuante em atividades da instituição', 'bruno.alcantara@fatec.sp.gov.br', 'img/uploads/fotos/default.png'),
+(15, 'Aluna colaborativa e presente nas discussões de sala', 'carolina.vieira@fatec.sp.gov.br', 'img/uploads/fotos/default.png');
 
 
 -- candidatos nas votações 
 INSERT INTO itens_votacao (id_votacao, id_cand)
 VALUES
 (1, 1), -- Votação 1 (DSM 2º semestre) -> Victor
-(1, 2), -- Votação 1 (DSM 2º semestre) -> Matheus
 (1, 3), -- Votação 1 (DSM 2º semestre) -> Feliphe
-(1, 4); -- Votação 1 (DSM 2º semestre) -> Marcos
+(1, 4), -- Votação 1 (DSM 2º semestre) -> Marcos
+(1, 2),  -- Luís
+(1, 6),  -- Mariana
+(1, 7),  -- Lucas
+
+(2, 8),  -- Gabriel
+(2, 9),   -- Isabela
+(2, 10),  -- Rafael
+(2, 11),  -- Fernanda
+(2, 12);  -- Eduardo
+
+
 
 
